e18166c36a5b0b5ec8f3b5d09fa4b762b7e529b8
barry-kn <carrybarryklein49@gmail.com>
2023-04-21 19:44:01 +0300

    sec commit

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+function ArticleList(props) {
+    const articleComponents = props.articles.map((article) => (
+      <Article
+        key={article.id}
+        title={article.title}
+        date={article.date ? article.date : 'January 1, 1970'}
+        preview={article.preview}
+      />
+    ));
+  
+    return <main>{articleComponents}</main>;
+  }
+  
+
+export default  ArticleList;
